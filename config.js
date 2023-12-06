05128523f48efa4d8fafc701d396a124550d6000
ΛＳＬΛ Ｍ <unknownmansion321@gmail.com>
2023-12-06 08:10:06 +0530

    Update config.js

--- config.js
+++ config.js
@@ -51,7 +51,7 @@ global.APIKeys = { // APIKey Here
 // Sticker WM
 global.botname = '𝟗𝑖𝑛𝑒 𝑥𝟑'
 global.premium = 'true'
-global.packname = '𝟗𝑖𝑛𝑒 𝑥𝟑' 
+global.packname = '𝑆𝑡𝑟𝑎𝑖𝑔ℎ𝑡 𝑂𝑢𝑡𝑡𝑎 𝐻𝑒𝑙𝑙' 
 global.author = ' 𝑓𝑎𝑙𝑠𝑒 𝑒𝑦𝑒 :)' 
 global.menuvid = 'https://telegra.ph/file/ca015bad793bcde1cd232.mp4'
 global.igfg = '𒈞 Follow on Instagram\nhttps://www.instagram.com/false_eye\n' 
