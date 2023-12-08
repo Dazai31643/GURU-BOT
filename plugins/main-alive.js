7bbe269cee93392e706c88c2b605dbb1df553e9d
ΛＳＬΛ Ｍ <unknownmansion321@gmail.com>
2023-12-08 09:56:20 +0530

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -4,8 +4,8 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
 
     // Sound
     let name = m.pushName || conn.getName(m.sender)
-    var vn = "https://raw.githubusercontent.com/false-eye/Xlicon-v2/master/Assets/audio/@919539901834.mp3"
-    let url = "https://github.com/false-eye/"
+    var vn = "https://cdn.jsdelivr.net/gh/false-eye/GURU-BOT@main/Assets/mp3/Alive.mp3"
+    let url = "https://github.com/false-eye"
     let murl = "https://youtu.be/iI34LYmJ1Fs?si=Cz3j9daLBo6VU2vF"
     let img = "https://i.imgur.com/M3p0NBV.jpeg"
     let con = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
@@ -13,16 +13,16 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
         audio: {
           url: vn
         },
-        mimetype: 'audio/mp4',
+        mimetype: 'audio/mpeg',
         ptt: true,
         waveform:  [100, 0, 100, 0, 100, 0, 100],
-        fileName: "Guru",
+        fileName: "𝟗𝑖𝑛𝑒 𝑥𝟑",
     
         contextInfo: {
           mentionedJid: [m.sender],
           externalAdReply: {
           title: "𝑁𝑜𝑡 𝐷𝑒𝑎𝑑 :)",
-          body: "𝟗𝑖𝑛𝑒 𝑥𝟑  ×  𝑓𝑎𝑙𝑠𝑒 𝑒𝑦𝑒",
+          body: "𝟗𝑖𝑛𝑒 𝑥𝟑 × 𝑓𝑎𝑙𝑠𝑒 𝑒𝑦𝑒",
           thumbnailUrl: img,
           sourceUrl: 'https://t.me/sarca_stics',
           mediaType: 1,
