52e355519d31d95ee0286c03f2e6ad1e22664011
ΛＳＬΛ Ｍ <unknownmansion321@gmail.com>
2024-01-04 20:13:35 +0530

    Update dl-play.js

--- plugins/dl-play.js
+++ plugins/dl-play.js
@@ -19,7 +19,7 @@ const handler = async (m, {
     conn.GURUPLAY = conn.GURUPLAY ? conn.GURUPLAY : {};
     await conn.reply(m.chat, wait, m);
     const result = await searchAndDownloadMusic(text);
-    const infoText = `✦ ──『 *𝟗𝑖𝑛𝑒 𝑥𝟑 𝑃𝑙𝑎𝑦𝑒𝑟* 』── ✦`;
+    const infoText = `✦ ──『 *𝟗𝑖𝑛𝑒 𝑥𝟑* 』── ✦ \n\n [ ⭐ Reply the number of the desired search result to get the Audio]. \n\n` ;
 
 const orderedLinks = result.allLinks.map((link, index) => {
     const sectionNumber = index + 1;
@@ -57,7 +57,7 @@ handler.before = async (m, {
         key,
         timeout
     } = conn.GURUPLAY[m.sender];
-    console.log(conn.GURUPLAY)
+   
     if (!m.quoted || m.quoted.id !== key.id || !m.text) return;
     const choice = m.text.trim();
     const inputNumber = Number(choice);
