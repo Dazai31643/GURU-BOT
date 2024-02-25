980cdd91d91545dea542e15069842bbfeff6604b
ΛＳＬΛ Ｍ <unknownmansion321@gmail.com>
2024-02-26 01:20:54 +0530

    Update main-alive.js

--- plugins/main-alive.js
+++ plugins/main-alive.js
@@ -7,7 +7,7 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
     var vn = "https://cdn.jsdelivr.net/gh/false-eye/GURU-BOT@main/Assets/mp3/Alive.mp3"
     let url = "https://github.com/false-eye"
     let murl = "https://youtu.be/iI34LYmJ1Fs?si=Cz3j9daLBo6VU2vF"
-    let img = "https://i.imgur.com/P1rwjDn.jpg"
+    let img = "https://i.imgur.com/LyUFu5Q.jpeg"
     let con = { key: { fromMe: false, participant: `${m.sender.split`@`[0]}@s.whatsapp.net`, ...(m.chat ? { remoteJid: '16504228206@s.whatsapp.net' } : {}) }, message: { contactMessage: { displayName: `${name}`, vcard: `BEGIN:VCARD\nVERSION:3.0\nN:;a,;;;\nFN:${name}\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`}}}
     let doc = {
         audio: {
@@ -16,13 +16,13 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
         mimetype: 'audio/mpeg',
         ptt: true,
         waveform:  [100, 0, 100, 0, 100, 0, 100],
-        fileName: "𝟗ine x𝟑",
+        fileName: "9ine.x3",
     
         contextInfo: {
           mentionedJid: [m.sender],
           externalAdReply: {
-          title: "𝑁𝑜𝑡 𝐷𝑒𝑎𝑑 :)",
-          body: "𝟗ine x𝟑 × false eye",
+          title: "still here :)",
+          body: "9ine.x3 by false eye",
           thumbnailUrl: img,
           sourceUrl: 'https://t.me/sarca_stics',
           mediaType: 1,
